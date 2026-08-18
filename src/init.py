508933756b1971c4d212370fc1461ea210b5414f
Valentina Shumakova <vshumakova01@gmail.com>
2026-08-18 22:55:03 +0300

    Create init.py

---
+++ src/init.py
@@ -0,0 +1,10 @@
+"""
+src - вспомогательные модули для анализа A/B-тестирования
+"""
+
+from . import data_loader
+from . import stats
+from . import metrics
+from . import visualizations
+
+__all__ = ['data_loader', 'stats', 'metrics', 'visualizations']
